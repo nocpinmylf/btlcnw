6836678da3ebec68e281c7da5b2f1ede89156650
nocpinmylf <lemduc@hotmail.com>
2021-10-26 09:02:22 +0700

    update product-list, add pagination

--- product-list.php
+++ product-list.php
@@ -20,24 +20,32 @@
         <nav class="selector">
           <!-- price-range-slider -->
           <div class="price-range-slider">
-            <div id="slider-range" class="range-bar" min ="0" max="500"></div>
+            <div id="slider-range" class="range-bar"></div>
             <p class="range-value">
               <input type="number" id="amountMin" placeholder="Tối thiểu" min="0" value="0">
               <span>-</span>
-              <input type="number" id="amountMax" placeholder="Tối đa" min="1" value="500">
+              <input type="number" id="amountMax" placeholder="Tối đa" min="1" value="5000">
             </p>
           </div>
           <!-- catergory -->
-          <select name="catergory" id="catergory">
-            <option value="all">Tất Cả</option>
-          </select>
+          <div class="selector-part">
+            <span>Danh Mục</span>
+            <select name="catergory" id="catergory">
+              <option value="all">Tất Cả</option>
+            </select>
+          </div>
+          
           <!-- sort -->
-          <select name="sort" id="sort">
-            <option value="priceUp">Giá: Tăng Dần</option>
-            <option value="priceDown">Giá: Giảm Dần</option>
-            <option value="nameUp">Tên: A - Z</option>
-            <option value="nameDown">Tên: Z - A</option>
-          </select>
+          <div class="selector-part">
+            <span>Sắp Xếp</span>
+            <select name="sort" id="sort">
+              <option value="priceUp">Giá: Tăng Dần</option>
+              <option value="priceDown">Giá: Giảm Dần</option>
+              <option value="nameUp">Tên: A - Z</option>
+              <option value="nameDown">Tên: Z - A</option>
+            </select>
+          </div>
+          
         </nav>
       </div>
       <div class="product-list-body">
@@ -47,13 +55,18 @@
           ProductItem('item3.jpg', 'Bộ Vi Xử Lý AMD Ryzen 3', 'AMD Ryzen 3 3200G /6MB /3.6GHz /4 nhân 4 luồng', 5, 159, 0);
           ProductItem('item4.jpg', 'Bộ Vi Xử Lý Core AMD Ryzen 9', 'AMD Ryzen 9 3900x /70MB /3.8GHz /12 nhân 24 luồng', 5, 519, 0);
 
-          ProductItem('item1.jpg', 'Bộ Vi Xử Lý Core i5', 'Core i5 9400 / 9M / 2.9GHz upto 4.1GHz / 6 nhân 6 luồng', 5, 139, 0);
-          ProductItem('item2.jpg', 'Bộ Vi Xử Lý AMD', 'AMD Athlon™ 200GE 3.2GHz / 2 nhân 4 luồng / Radeon™ Vega 3 Graphics', 4, 99, 0);
-          ProductItem('item3.jpg', 'Bộ Vi Xử Lý AMD Ryzen 3', 'AMD Ryzen 3 3200G /6MB /3.6GHz /4 nhân 4 luồng', 5, 159, 0);
-          ProductItem('item4.jpg', 'Bộ Vi Xử Lý Core AMD Ryzen 9', 'AMD Ryzen 9 3900x /70MB /3.8GHz /12 nhân 24 luồng', 5, 519, 0);
+          ProductItem('Gaming1.png', 'Sony Playstation 4 Pro 1TB Party Bundle', 'PlayStation 4 Pro (Jet Black) with 1TB HDD x 1 (CUH-7218BB01), DUALSHOCK 4 wireless controller (Jet Black) x 2', 5, 499, 0);
+          ProductItem('Gaming2.png', 'Tai nghe SteelSeries Arctis Pro Wireless', 'Độ nhạy: 102 dBSPL, Micro : -38 dBV/Pa', 4, 389, 0);
+          ProductItem('Gaming3.jpg', 'Chuột Razer Basilisk Ultimate', '20000DPI', 4, 239, 0);
+          ProductItem('Gaming4.png', 'Ghế Noble Chair Hero Series Real Leather', 'Chất liệu: Da thật', 5, 1259, 0);
+
+          ProductItem('other1.png', 'Bộ Dây Nguồn Custom Corsair Premium Type 4 Gen 4 – Blue', 'Bộ Dây Nguồn Custom Corsair Premium Type 4 Gen 4 – Blue', 4, 169, 0);
+          ProductItem('other2.png', 'Bộ Dây Nguồn Custom Corsair Premium Type 4 Gen 4 – White', 'Bộ Dây Nguồn Custom Corsair Premium Type 4 Gen 4 – White', 4, 169, 0);
+          ProductItem('other3.jpg', 'Balo RAZER ROUGE BACKPACK (15.6 Inch)', 'Balo RAZER ROUGE BACKPACK (15.6 Inch)', 5, 189, 0);
+          ProductItem('other4.jpg', 'Bao Silicon cho Tai nghe Sony WF-1000XM3', 'Bao Silicon cho Tai nghe Sony WF-1000XM3', 5, 9.99, 0);
         ?>
       </div>
-      <div class="pagination"></div>
+      <div id="pagination"></div>
   </main>
   
     <!-- footer -->
